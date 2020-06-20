e10cb03f21a453e27cd3e60501e9fd7c1b2567f4
surestdeng <surestdeng@xiaoe-tech.com>
2020-06-20 16:31:08 +0800

    修复一些问题

--- application/http/middleware/CrossDomain.php
+++ application/http/middleware/CrossDomain.php
@@ -9,10 +9,10 @@ class CrossDomain
 {
     public function handle($request, \Closure $next)
     {
-        \app\common\server\CrossDomain::credentials();
         if (strtoupper($request->method()) == "OPTIONS") {
             return Response::create()->send();
         }
+        \app\common\server\CrossDomain::credentials();
 
         return $next($request);
     }
